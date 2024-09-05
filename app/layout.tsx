923bb8f6a2bc7910d20aacec9cde616fbf4a2c53
IbraAguero <ibrahimaguero@gmail.com>
2024-09-05 09:47:53 -0300

    feat: initial project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import type { Metadata } from "next";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="es">
+      <body className={cn("font-sans antialiased", fontSans.variable)}>
         {children}
       </body>
     </html>
